155c3cf40d946462c810c6f50078ff78f552eb06
BugHunter-X <askzenai@gmail.com>
2025-01-20 04:32:05 +0100

    Add bugSolution.kt file

---
+++ bugSolution.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val result = list.map { it * 2 }
+    println(result) // Output: [2, 4, 6, 8, 10]
+
+    //Use a mutable list if you need to modify it
+    val mutableList = mutableListOf(1,2,3)
+    mutableList.add(4)
+    mutableList.add(5)
+    val resultMutable = mutableList.map { it * 2 }
+    println(resultMutable) // Output: [2, 4, 6, 8, 10] 
+} 
